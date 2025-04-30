343d80803617769f4a0810a745b399e3f3653718
aahilalwani25 <alwani.aahil25@gmail.com>
2025-04-30 11:03:54 +0500

    feat: add folder structure

---
+++ src/screens/home/user-home.container.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserHomeView from './user-home.view'
+
+function UserHomeContainer() {
+  return (
+    <UserHomeView/>
+  )
+}
+
+export default UserHomeContainer
---
+++ src/screens/home/user-home.view.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function UserHomeView() {
+  return (
+    <View>
+        
+    </View>
+  )
+}
+
+export default UserHomeView
