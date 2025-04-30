9b725db0567226d8bd58cd9e30c130cce35d393a
aahilalwani25 <alwani.aahil25@gmail.com>
2025-04-30 11:04:25 +0500

    feat: add user home

---
+++ src/app/(dashboard)/(home)/home.tsx
@@ -0,0 +1,10 @@
+import UserHomeContainer from '@/src/screens/home/user-home.container'
+import React from 'react'
+
+function Home() {
+  return (
+    <UserHomeContainer/>
+  )
+}
+
+export default Home
